e395c86101fd47c2ff98f4dcc99165718e24eb65
Sahinur Islam <infosahinur@gmail.com>
2023-07-20 05:28:11 +0600

    [ADD] sidebar

---
+++ components/sidebar.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { Montserrat } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+import { LayoutDashboard, MessageSquare, ImageIcon, VideoIcon, Music, Code, Settings } from "lucide-react";
+
+const montserrat = Montserrat({
+    weight: "600",
+    subsets: ["latin"]
+})
+
+const routes = [
+    {
+        label: "Dashboard",
+        icon: LayoutDashboard,
+        href: "/dashboard",
+        color: "text-sky-500",
+    },
+    {
+        label: "Conversation",
+        icon: MessageSquare,
+        href: "/conversation",
+        color: "text-violet-500",
+    },
+    {
+        label: "Image Generation",
+        icon: ImageIcon,
+        href: "/image",
+        color: "text-pink-700",
+    },
+    {
+        label: "Video Generation",
+        icon: VideoIcon,
+        href: "/video",
+        color: "text-orange-700",
+    },
+    {
+        label: "Music Generation",
+        icon: Music,
+        href: "/music",
+        color: "text-emerald-500",
+    },
+    {
+        label: "Code Generation",
+        icon: Code,
+        href: "/code",
+        color: "text-green-700",
+    },
+    {
+        label: "Settings",
+        icon: Settings,
+        href: "/settings",
+    },
+]
+
+
+const Sidebar = () => {
+    return (
+        <div className="space-y-4 py-4 flex flex-col h-full bg-gray-900 text-white">
+            <div className="px-3 py-2 flex-1">
+                <Link href={"/dashboard"} className="flex items-center pl-3 mb-14">
+                    <div className="relative w-14 h-8 mr-4">
+                        <Image
+                            fill
+                            alt="Logo"
+                            src="/logo.png"
+                        />
+                    </div>
+                    <h1 className={cn("text-2xl font-bold", montserrat.className)}>AI</h1>
+                </Link>
+                <div className="space-y-1">
+                    {routes.map((route, i) => (
+                        <Link
+                            href={route.href}
+                            key={route.href}
+                            className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+                        >
+                            <div className="flex items-center flex-1">
+                                <route.icon className={cn("w-5 h-5 mr-3", route.color)} />
+                                <span className="text-lg">{route.label}</span>
+                            </div>
+                        </Link>))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
