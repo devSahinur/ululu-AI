d75280b631c004f08855ef1516291fb3189965ff
Sahinur Islam <infosahinur@gmail.com>
2023-07-20 06:03:33 +0600

    [ADD] Dashborad

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,6 +3,7 @@
 import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import { LayoutDashboard, MessageSquare, ImageIcon, VideoIcon, Music, Code, Settings } from "lucide-react";
@@ -58,6 +59,10 @@ const routes = [
 
 
 const Sidebar = () => {
+
+    const pathname = usePathname()
+
+
     return (
         <div className="space-y-4 py-4 flex flex-col h-full bg-gray-900 text-white">
             <div className="px-3 py-2 flex-1">
@@ -76,7 +81,9 @@ const Sidebar = () => {
                         <Link
                             href={route.href}
                             key={route.href}
-                            className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+                            className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                            pathname === route.href ? "bg-white/10 text-white" : "text-gray-400"
+                            )}
                         >
                             <div className="flex items-center flex-1">
                                 <route.icon className={cn("w-5 h-5 mr-3", route.color)} />
